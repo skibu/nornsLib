aa6880c298b925f2e3743e2a396483e834966f27
Michael Smith <mike@goswift.ly>
2024-08-13 20:04:16 +0100

    Stripped down to just download

--- includeNornsLib.lua
+++ includeNornsLib.lua
@@ -1,34 +1,11 @@
--- For including libs from nornsLib repo. Similar to include(), but downloads 
--- nornsLib if haven’t done so previously to the user's device. And if NornsLib
--- already downloaded then this will automatically load updates via git checkout.
--- @tparam name - just the name of the particulae lib to include. Don't need
--- directory nor the .lua suffix.
-function include_norns_lib(name)
-  -- Where to find the github github_repo
-  local github_repo_owner = "skibu"
-  local github_repo = "nornsLib"
+-- The code for manually downloading NornsLib is shown below. This is for when an
+-- application script wants to make sure that nornsLib is available. That means
+-- that this script will not be available if it hasn't yet been downloaded. 
+-- Therefore an application script should copy this function into their code and
+-- then call it before trying to include any of the nornsLib utilities.
 
-  -- Update or download the library
-  if util.file_exists(_path.code..github_repo) then
-    -- Norns lib already exists so just update it. Need to do a "checkout ."
-    -- to get missing files and a "pull" to get modifications.
-    -- NOTE: user made changes to the lib will be lost!
-    local checkout_command = "git -C ".._path.code..github_repo.." checkout ."
-    local pull_command = "git -C ".._path.code..github_repo.." pull --ff-only"
-    print("Updating NornsLib using command:\n"..checkout_command.."\nand\n"..pull_command)
-    os.execute(checkout_command)
-    os.execute(pull_command)
-  else
-    -- Norns lib hasn't yet been downloaded so clone it
-    local command = "git clone https://github.com/"..github_repo_owner.."/"..github_repo..".git "..
-      _path.code..github_repo
-    print("Downloading NornsLib using command:\n"..command)
-    os.execute(command)
+function download_nornsLib()
+  if not util.file_exists(_path.code.."nornsLib") then
+    os.execute("git clone https://github.com/skibu/nornsLib.git ".._path.code.."nornsLib")
   end
-  
-  -- Now try including the lib again
-  return include(github_repo.."/"..name)
 end
-
--- Then can include lib files vie something like:
-include_norns_lib("parameterExtensions")
