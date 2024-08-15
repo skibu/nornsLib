d54882504c4329454cc2125b2ff47b2ede5e03f3
Michael Smith <mike@goswift.ly>
2024-08-15 03:44:52 +0100

    Cleaning up the example files

---
+++ fullNornsLibInclude.lua
@@ -0,0 +1,17 @@
+-- This file shows how nornsLib can be included. It is recommended that the 
+-- application developer copy this file to their application, modify as needed,
+-- and then include it. 
+
+-- If nornsLib doesn't exist on user's Norns device then clone it from GitHub
+if not util.file_exists(_path.code.."nornsLib") then
+  os.execute("git clone https://github.com/skibu/nornsLib.git ".._path.code.."nornsLib")
+end
+
+-- Include this file if app should auto update nornsLib to pick up the latest
+-- greatest version. Now that we know that at least an old verison nornsLib 
+-- already installed we can include it from the nornsLib directory.
+include "nornsLib/updateLib"
+
+-- Include the appropriate nornsLib extensions. Or more easily, just load all of them
+-- at once using includeAllExt.
+include "nornsLib/includeAllExt"
