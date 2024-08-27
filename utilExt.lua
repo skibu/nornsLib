15db264199b258cd55981492cdbfde02a8d43a3c
Michael Smith <mike@goswift.ly>
2024-08-27 04:22:38 +0100

    Changed util.tprint() to use truncated result of util.time()

--- utilExt.lua
+++ utilExt.lua
@@ -23,7 +23,7 @@ end
 
 -- Retuns epoch time string with with nanosecond precision, by doing a system 
 -- call. Note that because the number of characters one cannot just convert this
--- to a number via tonumber() because would then loose resolution.
+-- to a number via tonumber() because would then lose resolution.
 function util.epochtime_str()
   return util.execute_command("date +%s.%N")
 end
@@ -32,14 +32,10 @@ end
 -- print(), but puts the epoch time in front. Really nice for understanding what
 -- parts of your code are taking a long time to execute and need to be 
 -- optimized. The time is shortened to only show 4 digits to left of decimal point,
--- and 6 digits to the right. Showing more would just be kind of ugly. Nano seconds
--- are just truncated instead of rounded because that level of precision is not
--- actually useful for print statements.
+-- and 4 digits to the right. Showing more would just be kind of ugly. 
 function util.tprint(obj)
-  local time_str = util.epochtime_str()
-  decimal_loc = string.find(time_str, "%.")
-  local truncated_time_str = string.sub(time_str, decimal_loc-4, decimal_loc+6)
-  print(truncated_time_str .. " - " .. tostring(obj))
+  local truncated_time = (math.floor(util.time()*10000) % 100000000)/10000.0
+  print(truncated_time .. " - " .. tostring(obj))
 end
 
 
@@ -47,6 +43,13 @@ end
 -- Great for debugging.
 function util.debug_tprint(obj)
   if debug_mode then util.tprint("Debug: "..obj) end
+end
+
+
+-- Does a regular print(), but only if the global debug_mode is set to true.
+-- Great for debugging.
+function util.debug_print(obj)
+  if debug_mode then print("Debug: "..obj) end
 end
 
 
@@ -183,11 +186,18 @@ local function _wait_for_file_callback(stage, mtro)
 end
 
 
--- Waits until the file specified exists and is not changing in size. Uses an 
--- available metro timer, and frees it once done.
--- Recommend a tick_time of 0.1 to 0.2 seconds. 
+-- Waits until the file specified exists and is not changing in size. At that
+-- point the callback is called. Uses an available metro timer, and frees it
+-- once done. Recommend a tick_time of 0.1 to 0.2 seconds. 
 -- max_time specifies how long should wait. Must be at least 1.0 second.
 function util.wait(full_filename, file_available_callback, tick_time, max_time)
+  -- If file already exists and is not empty then call the callback immediately
+  if util.file_exists(full_filename) and util.file_size(full_filename) > 0 then
+    util.debug_tprint("File already available for file="..full_filename)
+    file_available_callback(full_filename)
+    return
+  end
+  
   local count = max_time / tick_time
   
   wait_metro = metro.init(_wait_for_file_callback, tick_time, count)
