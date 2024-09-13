68a061cff4aa7cf9dd87910571988d994947c236
Michael Smith <mike@goswift.ly>
2024-09-13 05:14:25 +0100

    Enhanced debug print funcs so they display context

--- utilExt.lua
+++ utilExt.lua
@@ -50,23 +50,54 @@ function util.tprint(obj)
 end
 
 
+-- Provides context of when the debug print statement was called
+local function debug_line(called_by_print)
+  -- Number of levels that need to go up when calling debug.getinfo() depends
+  -- on whether an extra layer of print() functions used.
+  local levels_up = called_by_print == nil and 3 or 4
+  
+  local debug_info = debug.getinfo(levels_up, "Sln")
+  
+  local function_name = debug_info.name
+  if function_name ~= nil then 
+    function_name = function_name .. "() "
+  else  
+    function_name = "" 
+  end
+  
+  return "DEBUG " .. function_name .. util.get_filename(debug_info.short_src) ..
+    " line:"..debug_info.currentline
+end
+
+
 -- Does a util.tprint(), but only if the global debug_mode is set to true.
--- Great for debugging.
-function util.debug_tprint(obj)
-  if debug_mode then util.tprint("Debug: "..obj) end
+-- Great for debugging. Parameter called_by_print should be set to non nil 
+-- if this function was called by another util.print() function. This is
+-- needed so that the proper function will be displayed in the message.
+function util.debug_tprint(obj, called_by_print)
+  -- Don't do anything if not in debug mode
+  if debug_mode ~= true then return end
+    
+  -- Output the info  
+  util.tprint(debug_line(called_by_print).."\n            "..obj)
 end
 
 
 -- Since util.debug_tprint(obj) is so useful here is a shorter name for it
 function util.dprint(obj)
-  util.debug_tprint(obj)
+  -- Note: setting called_by_print to true since calling an extra layer of print function
+  util.debug_tprint(obj, true)
 end
 
 
 -- Does a regular print(), but only if the global debug_mode is set to true.
 -- Great for debugging.
 function util.debug_print(obj)
-  if debug_mode then print("Debug: "..obj) end
+  -- Don't do anything if not in debug mode
+  if debug_mode ~= true then return end
+    
+  -- Output the info  
+  print(debug_line().."\n"..obj)
 end
 
 
